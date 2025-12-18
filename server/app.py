e7ea97f1951fc55a7fb875e9e6e015c4bf58516e
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-12-18 12:47:32 -0600

    Complete Flask-SQLAlchemy earthquake API

--- server/app.py
+++ server/app.py
@@ -21,7 +21,43 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    # query the database for an earthquake with this id
+    earthquake = Earthquake.query.filter_by(id=id).first()
 
+    # if no earthquake is found, return a 404 error
+    if earthquake is None:
+        return {"message": f"Earthquake {id} not found."}, 404
+
+    # return the earthquake data as JSON
+    return {
+        "id": earthquake.id,
+        "location": earthquake.location,
+        "magnitude": earthquake.magnitude,
+        "year": earthquake.year
+    }, 200
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    # query earthquakes with magnitude greater than or equal to the given value
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    # build a list of earthquake data
+    quakes = []
+    for earthquake in earthquakes:
+        quakes.append({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        })
+
+    # return the count and the list of earthquakes
+    return {
+        "count": len(quakes),
+        "quakes": quakes
+    }, 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,3 +6,24 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+# Earthquake model
+class Earthquake(db.Model):
+    # table name
+    __tablename__ = "earthquakes"
+
+    # primary key
+    id = db.Column(db.Integer, primary_key=True)
+
+    # magnitude of the earthquake
+    magnitude = db.Column(db.Float)
+
+    # location of the earthquake
+    location = db.Column(db.String)
+
+    # year the earthquake happened
+    year = db.Column(db.Integer)
+
+    # string representation of the object
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
